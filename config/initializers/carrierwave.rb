2ce67f6cf74b3e054d2268ecf2ce103b8380e130
shinji <sshin8916@gmail.com>
2020-03-08 16:37:02 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,18 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY']
+    }
 
-  config.fog_directory  = 'share-diary0916'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/share-diary0916'
+    config.fog_directory  = ENV['AWS_S3_BUCKET']
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/share-diary0916'
+  end
 end
